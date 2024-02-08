4beace56d0d6e01cf0369f0f79ff9adbdd38976a
Zebix77 <157887733+Zebix77@users.noreply.github.com>
2024-02-08 23:39:55 +0500

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,34 @@
+#FUNCTION DEFINATION:
+# def to_do(a,b): #PARAMETERS
+#     return(a**b)
+# ex= to_do(5,5) #FUNCTION CALL
+# print(ex)
+
+# def average(a,b,c):
+#     return(a+b+c)//3
+# storage = average(100,50,60)
+# print(storage)
+
+# a = [10,20,30,40,50,60,70,80,90,100]
+# for  n in range(0,9,2): 
+#     print(a[n])
+ 
+# def prod(a,b):
+#     return(a*b)
+# ans = prod(100,100)
+# print(ans)
+
+# x = ["I", "Live", "In", "pakistan", "my", "name", "is",]
+# # def len_x(list):
+# #     print(len(list))
+# # len_x(x)
+
+# def print_len(x):
+#     for item in x:
+#         print(item,end=" ")
+
+# def avg_cal(a,b,c):
+#     return(a+b+c)//3
+# average=avg_cal(98,97,95)
+# print(average)
+ 
--- Loops.py
+++ Loops.py
@@ -19,3 +19,62 @@
 #     print("Zebi")
 #     x=x+5
 
+
+# x = 1 
+# n = eval(input("Enter the number:"))
+# while x <=10:
+#     print(n*x)
+#     x+=1 
+
+# x = [1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while idx < len(x):
+#     print(x[idx])
+#     idx += 1
+
+# num = (10,20,30,40,50,60,70,80,90,100)
+# x = 90
+# idx = 0
+# while idx < len(num):
+#     if (num[idx]) == x:
+#         print("Found at", idx)
+#     idx += 1
+
+# i = 0
+# while i<=5:
+#     if (i%2 == 0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+ 
+
+# cars = ["Audi", "BMW", "Mercedes", "VW", "Porsche", "Lamborghini"]
+# for x in cars:
+#     print(x, end= " ")
+
+
+# num = (1,2,3,4,5,6,7,8,9,10,7,90,7)
+# x = 7
+# idx = 0
+# for n in num:
+#     if n == x:
+#         print("Found at idx:", idx)
+#     idx +=1
+
+
+
+#FIND FACTORIAL USING WHILE LOOP
+# n = 5
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+# print("Answer is:" , fact)
+
+n = int(input("Enter the number:"))
+fact = 1
+for i in range(1,n+1):
+    fact *= i 
+    print("Factorial:", fact)
