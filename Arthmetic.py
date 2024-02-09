b25a2d3426335ddbf212579a76c38d4a50255382
Zebix77 <shahzaib41038@gmail.com>
2024-02-09 20:46:10 +0500

    Update:

--- Arthmetic.py
+++ Arthmetic.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # x=90
 # y=80
 # print(x%y)
@@ -16,22 +15,9 @@ list_iterate_char.reverse()
 print(list_iterate_char)  
 
 
-# x = ["3cats", "4dogs"]
-# list_iterate_char = [(x)[0][1:], (x)[1][1:]] 
-# list_iterate_char.reverse()  
-# print(list_iterate_char)  
-
-
-
 
 
 
 
 # list_iterate_int = int(x[0][0]), int(x[1][0])
 # multiply = list_iterate_int[0] * 2, list_iterate_int[1] * 2
-=======
-x=90
-y=80
-print(x%y)
-print(x/y)
->>>>>>> 4beace56d0d6e01cf0369f0f79ff9adbdd38976a
