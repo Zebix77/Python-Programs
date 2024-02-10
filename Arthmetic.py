58f27e57f2812b62cd4c4589bc88ad6ffb6b115d
Zebix77 <shahzaib41038@gmail.com>
2024-02-10 21:29:35 +0500

    function example

--- Arthmetic.py
+++ Arthmetic.py
@@ -11,13 +11,28 @@
 
 x = ["3cats", "4dogs"]
 list_iterate_char = [(x)[0][1:], (x)[1][1:]]  
-list_iterate_char.reverse()  
-print(list_iterate_char)  
+
+list_iterate_int = int(x[0][0]), int(x[1][0])
+multiply = list_iterate_int[0] * 2, list_iterate_int[1] * 2
+print(multiply)
+
+
+# list_iterate_char.reverse()  
+# print(list_iterate_char)  
+# list_iterate_char.reverse()  
+# print(list_iterate_char)
+
+# def zebifunction (inList):
+#     inList.reverse()
+#     return inList
+
+# newList = [2,4,6,6,8]
+# secondList = ["a", "b", "c", "d"]
+
+# print(zebifunction(secondList))
 
 
 
 
 
 
-# list_iterate_int = int(x[0][0]), int(x[1][0])
-# multiply = list_iterate_int[0] * 2, list_iterate_int[1] * 2
